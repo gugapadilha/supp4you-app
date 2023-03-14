d1a259cd88ea08b2a0a836f5ce23e2c8e119f455
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-03-14 17:51:05 -0300

    change findviewbyid for viewbinding intead on registerFragment

--- app/src/main/java/com/guga/supp4youapp/RegisterFragment.kt
+++ app/src/main/java/com/guga/supp4youapp/RegisterFragment.kt
@@ -5,17 +5,19 @@ import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.guga.supp4youapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    private lateinit var binding: FragmentRegisterBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<TextView>(R.id.tv_login).setOnClickListener {
+        binding = FragmentRegisterBinding.bind(view)
+
+        binding.tvLogin.setOnClickListener {
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
-
-
     }
-
 }
