1847ef5f0832a328412a729ab31fedee34da6f3e
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-03-14 17:45:05 -0300

    changing findviewvbyid for viewbinding instead

--- app/src/main/java/com/guga/supp4youapp/LoginFragment.kt
+++ app/src/main/java/com/guga/supp4youapp/LoginFragment.kt
@@ -1,20 +1,25 @@
 package com.guga.supp4youapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.guga.supp4youapp.databinding.FragmentLoginBinding
 
-class LoginFragment : Fragment(R.layout.fragment_login){
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<TextView>(R.id.tv_login).setOnClickListener {
+        binding = FragmentLoginBinding.bind(view)
+
+        binding.tvLogin.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
-
-
     }
 }
