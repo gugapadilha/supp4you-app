891f8db9b15e508be67a47f95f019ea94eb380c3
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-03-15 16:59:42 -0300

    add spinner implementation on code

--- app/src/main/java/com/guga/supp4youapp/presentation/ui/DetailsFragment.kt
+++ app/src/main/java/com/guga/supp4youapp/presentation/ui/DetailsFragment.kt
@@ -1,17 +1,45 @@
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.guga.supp4youapp.R
 import com.guga.supp4youapp.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
 
-    private lateinit var binding: FragmentDetailsBinding
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding = FragmentDetailsBinding.bind(view)
+        val spinnerItems = listOf("ULBRA", "UNESC", "UNIASSELVI", "UCS")
+        val spinnerAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerItems)
+        binding.spinner.adapter = spinnerAdapter
 
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+                // do something with selected value
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // do the validation
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
