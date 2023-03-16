fc0ab76cbe90c46b432f6b24a3844ebe08ac66ce
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-03-16 13:50:10 -0300

    user session added

---
+++ app/src/main/java/com/guga/supp4youapp/domain/model/UserSession.kt
@@ -0,0 +1,25 @@
+package com.guga.supp4youapp.domain.model
+
+
+object UserSession {
+
+    private var _token: String? = null
+
+    fun isUserLogged(): Boolean {
+        return _token != null
+    }
+
+    fun setToken(token: String) {
+        _token = token
+    }
+
+    fun cleanSession() {
+        _token = null
+    }
+
+    fun getToken(): String = if (_token != null)
+        _token!!
+    else
+        ""
+
+}
