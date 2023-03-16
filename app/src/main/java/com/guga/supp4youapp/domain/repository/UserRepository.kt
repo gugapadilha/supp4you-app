5847019e749d3189d9c7a902dfa7b316e2e94d37
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-03-16 14:09:32 -0300

    user repository done

---
+++ app/src/main/java/com/guga/supp4youapp/domain/repository/UserRepository.kt
@@ -0,0 +1,12 @@
+package com.guga.supp4youapp.domain.repository
+
+import com.guga.supp4youapp.domain.model.LoginRequest
+import com.guga.supp4youapp.domain.model.User
+import com.guga.supp4youapp.domain.rest.RetrofitService
+
+class UserRepository constructor(private val retrofitService: RetrofitService) {
+
+    fun saveUser(user: User) = retrofitService.saveUser(user)
+
+    fun login(loginRequest: LoginRequest) = retrofitService.login(loginRequest)
+}
