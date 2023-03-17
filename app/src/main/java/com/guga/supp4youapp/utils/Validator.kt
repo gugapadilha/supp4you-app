60fefb4fcbe8991c2453c12a98803bf911adeb31
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-03-17 16:49:34 -0300

    validate token code

--- app/src/main/java/com/guga/supp4youapp/utils/Validator.kt
+++ app/src/main/java/com/guga/supp4youapp/utils/Validator.kt
@@ -12,4 +12,15 @@ object Validator {
         return !(name.isEmpty() || name.isBlank())
     }
 
+    fun validateCode(){
+        val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        val codeSize = 6
+        val random = java.util.Random()
+        val code = CharArray(codeSize)
+
+        for (i in 0 until codeSize) {
+            code[i] = letters[random.nextInt(letters.length)]
+        }
+    }
+
 }
