c40547ce7d599c329f9d7b46d58007da3290c7b0
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-03-21 16:43:04 -0300

    creating and injecting hilt to viewmodel

--- app/src/main/java/com/guga/supp4youapp/presentation/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/guga/supp4youapp/presentation/ui/login/LoginViewModel.kt
@@ -1,11 +1,19 @@
 package com.guga.supp4youapp.presentation.ui.login
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.guga.supp4youapp.domain.model.LoginRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor() : ViewModel() {
 
+    private val _data = MutableLiveData<List<LoginRequest>>()
+    val data: MutableLiveData<List<LoginRequest>> = _data
+
+    fun validateLogin(){
+
+    }
 
 }
