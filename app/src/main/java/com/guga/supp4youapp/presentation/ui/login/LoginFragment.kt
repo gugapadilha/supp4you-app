f42b587ed83b18565c1251063ea32a1052379d9f
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-03-21 17:14:19 -0300

    passing validateLogin to viewmodel

--- app/src/main/java/com/guga/supp4youapp/presentation/ui/login/LoginFragment.kt
+++ app/src/main/java/com/guga/supp4youapp/presentation/ui/login/LoginFragment.kt
@@ -4,47 +4,36 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.guga.supp4youapp.R
-import com.guga.supp4youapp.data.DataSourceLogin
 import com.guga.supp4youapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var binding: FragmentLoginBinding
+    private val loginViewModel: LoginViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentLoginBinding.bind(view)
 
-        fun validateLogin() {
+        binding.tvCreatenow.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.tvLogin.setOnClickListener {
             val username = binding.edEmail.text.toString()
             val password = binding.tvPassword.text.toString()
 
-            val loginDataSet = DataSourceLogin.createDataSetLogin()
-
-            var isLoginSuccessful = false
-            for (loginRequest in loginDataSet) {
-                if (loginRequest.value?.email == username && loginRequest.value?.password == password) {
-                    isLoginSuccessful = true
-                    break
-                }
-            }
+            val isLoginSuccessful = loginViewModel.validateLogin(username, password)
 
             if (isLoginSuccessful) {
                 findNavController().navigate(R.id.action_loginFragment_to_detailsFragment)
             } else {
                 Toast.makeText(requireContext(), "Invalid Credencials", Toast.LENGTH_SHORT).show()
             }
-        }
-
-        binding.tvCreatenow.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
-        }
-
-        binding.tvLogin.setOnClickListener {
-            validateLogin()
         }
 
     }
