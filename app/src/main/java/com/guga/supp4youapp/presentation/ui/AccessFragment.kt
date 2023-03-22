2181ae9bfdf1eca7cd2029de6bd1d52d7b9ddedb
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-03-22 17:00:41 -0300

    validate code on access done

--- app/src/main/java/com/guga/supp4youapp/presentation/ui/AccessFragment.kt
+++ app/src/main/java/com/guga/supp4youapp/presentation/ui/AccessFragment.kt
@@ -1,6 +1,8 @@
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.guga.supp4youapp.R
 import com.guga.supp4youapp.databinding.FragmentAccessBinding
 
@@ -13,5 +15,17 @@ class AccessFragment : Fragment(R.layout.fragment_access) {
 
         binding = FragmentAccessBinding.bind(view)
 
+        binding.tvEnterspace.setOnClickListener {
+            //navigation to next screen here
+        }
+
+        fun validateCode(){
+            if(binding.tvEntercode.text.toString().isBlank() || binding.tvEntercode.text?.length != 6){
+                Toast.makeText(requireContext(), "Invalid Credencials", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //navigation here
+            }
+        }
     }
 }
