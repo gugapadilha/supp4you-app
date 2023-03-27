acf2a69dc96de74bbccf1a077b67e6d8b2f774b4
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-03-27 17:58:04 -0300

    generate code working as expected

--- app/src/main/java/com/guga/supp4youapp/presentation/ui/GenerateFragment.kt
+++ app/src/main/java/com/guga/supp4youapp/presentation/ui/GenerateFragment.kt
@@ -1,20 +1,43 @@
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Toast
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.guga.supp4youapp.R
-import com.guga.supp4youapp.databinding.FragmentAccessBinding
 import com.guga.supp4youapp.databinding.FragmentGenerateBinding
 
 class GenerateFragment : Fragment(R.layout.fragment_generate) {
 
-    private lateinit var binding: FragmentGenerateBinding
+    private var _binding: FragmentGenerateBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGenerateBinding.inflate(inflater, container, false)
+        val code = validateCode()
+        binding.tvEntercode.text = code
+        return binding.root
 
-        binding = FragmentGenerateBinding.bind(view)
+    }
 
+    private fun validateCode(): String {
+        val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        val codeSize = 6
+        val random = java.util.Random()
+        val code = CharArray(codeSize)
+
+        for (i in 0 until codeSize) {
+            code[i] = letters[random.nextInt(letters.length)]
+        }
+
+        return String(code)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
