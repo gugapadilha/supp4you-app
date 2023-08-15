510564b9afe22fed9e6d5a82a5e9de401c24cc45
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-08-15 19:33:36 -0300

    firebase add in project successfully

--- app/src/main/java/com/guga/supp4youapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/guga/supp4youapp/presentation/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.guga.supp4youapp.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
 import com.guga.supp4youapp.R
 
 class MainActivity : AppCompatActivity() {
