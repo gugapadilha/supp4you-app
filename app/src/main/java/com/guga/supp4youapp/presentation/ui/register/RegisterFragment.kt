a238c26fed61e7993086d30479ba827e190c3bb6
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-08-23 19:37:26 -0300

    icon visibility impl in register account

--- app/src/main/java/com/guga/supp4youapp/presentation/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/guga/supp4youapp/presentation/ui/register/RegisterFragment.kt
@@ -1,10 +1,14 @@
 package com.guga.supp4youapp.presentation.ui.register
 
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
 import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -45,13 +49,53 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
                 Toast.makeText(requireContext(), "Invalid Credencials", Toast.LENGTH_SHORT).show()
             }
         }
+        togglePasswordVisibility(binding.edPassword, binding.visibilityOff)
+        togglePasswordVisibility(binding.edRepeatPassword, binding.visibility)
+
+        binding.visibilityOff.setOnClickListener {
+            togglePasswordVisibility(binding.edPassword, binding.visibilityOff)
+        }
+
+        binding.visibility.setOnClickListener {
+            togglePasswordVisibilityOff(binding.edRepeatPassword, binding.visibility)
+        }
+
     }
+
+    private fun togglePasswordVisibility(passwordEditText: AppCompatEditText, visibilityTextView: AppCompatTextView) {
+        if (passwordEditText.transformationMethod == null) {
+            // A senha está visível, então tornamos oculta
+            passwordEditText.transformationMethod = PasswordTransformationMethod.getInstance()
+            visibilityTextView.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_off_24, 0)
+        } else {
+            // A senha está oculta, então tornamos visível
+            passwordEditText.transformationMethod = null
+            visibilityTextView.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_24, 0)
+        }
+        // Move o cursor para o final do texto
+        passwordEditText.setSelection(passwordEditText.text!!.length)
+    }
+
+    private fun togglePasswordVisibilityOff(passwordEditText: AppCompatEditText, visibilityTextView: AppCompatTextView) {
+        if (passwordEditText.transformationMethod == null) {
+            // A senha está visível, então tornamos oculta
+            passwordEditText.transformationMethod = PasswordTransformationMethod.getInstance()
+            visibilityTextView.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_off_24, 0)
+        } else {
+            // A senha está oculta, então tornamos visível
+            passwordEditText.transformationMethod = null
+            visibilityTextView.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_24, 0)
+        }
+        // Move o cursor para o final do texto
+        passwordEditText.setSelection(passwordEditText.text!!.length)
+    }
+
 
     private fun checkAllFields() : Boolean {
         val email = binding.edEmail.text.toString()
 
         if (binding.edEmail.text.toString() == ""){
-            Toast.makeText(requireContext(), "This is a required field", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Email is a required field", Toast.LENGTH_SHORT).show()
             return false
         }
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
@@ -59,15 +103,15 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
             return false
         }
         if (binding.edPassword.text.toString() == ""){
-            Toast.makeText(requireContext(), "This is a required field", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Password is a required field", Toast.LENGTH_SHORT).show()
             return false
         }
         if (binding.edPassword.length() <= 6) {
-            Toast.makeText(requireContext(), "Password should at least 6 characters long", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Password should at least 8 characters long", Toast.LENGTH_SHORT).show()
             return false
         }
         if (binding.edRepeatPassword.toString() == ""){
-            Toast.makeText(requireContext(), "This is a required field", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Repeat Password is a required field", Toast.LENGTH_SHORT).show()
             return false
         }
         if (binding.edPassword.text.toString() != binding.edRepeatPassword.text.toString()){
