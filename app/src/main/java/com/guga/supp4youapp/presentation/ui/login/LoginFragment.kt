d0c6c827506ae5f05feddbe2f7d591bbf841304d
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-08-23 19:44:40 -0300

    visibility icon on and off added in login as well

--- app/src/main/java/com/guga/supp4youapp/presentation/ui/login/LoginFragment.kt
+++ app/src/main/java/com/guga/supp4youapp/presentation/ui/login/LoginFragment.kt
@@ -2,7 +2,10 @@ package com.guga.supp4youapp.presentation.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -19,26 +22,33 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentLoginBinding.bind(view)
+        togglePasswordVisibility(binding.edPassword, binding.visibility)
 
         binding.tvCreatenow.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
 
         binding.tvLogin.setOnClickListener {
-//                val intent = Intent(requireContext(), CameraActivity::class.java)
-//                startActivity(intent)
             findNavController().navigate(R.id.action_loginFragment_to_detailsFragment)
-//            val username = binding.edEmail.text.toString()
-//            val password = binding.edPassword.text.toString()
-//
-//            val isLoginSuccessful = loginViewModel.validateLogin(username, password)
-//
-//            if (isLoginSuccessful) {
-//                findNavController().navigate(R.id.action_loginFragment_to_detailsFragment)
-//            } else {
-//                Toast.makeText(requireContext(), "Invalid Credencials", Toast.LENGTH_SHORT).show()
-//            }
         }
 
+        binding.visibility.setOnClickListener {
+            togglePasswordVisibility(binding.edPassword, binding.visibility)
+        }
+
+    }
+
+    private fun togglePasswordVisibility(passwordEditText: AppCompatEditText, visibilityTextView: AppCompatTextView) {
+        if (passwordEditText.transformationMethod == null) {
+            // A senha está visível, então tornamos oculta
+            passwordEditText.transformationMethod = PasswordTransformationMethod.getInstance()
+            visibilityTextView.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_off_24, 0)
+        } else {
+            // A senha está oculta, então tornamos visível
+            passwordEditText.transformationMethod = null
+            visibilityTextView.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_24, 0)
+        }
+        // Move o cursor para o final do texto
+        passwordEditText.setSelection(passwordEditText.text!!.length)
     }
 }
