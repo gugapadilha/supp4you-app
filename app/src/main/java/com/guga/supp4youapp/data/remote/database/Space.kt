5886d0a4ead581724d5e8959a7d8f13f81314f3f
gugapadilha <guga.santospadilha@rede.ulbra.br>
2023-08-29 14:33:52 -0300

    created tables in firestore

---
+++ app/src/main/java/com/guga/supp4youapp/data/remote/database/Space.kt
@@ -0,0 +1,8 @@
+package com.guga.supp4youapp.data.remote.database
+
+data class Space(
+   var groupName: String,
+   var selectDays: String,
+   var selectBeginTime: String,
+   var selectEndTime: String
+)
--- app/src/main/java/com/guga/supp4youapp/presentation/ui/fragment/AccessFragment.kt
+++ app/src/main/java/com/guga/supp4youapp/presentation/ui/fragment/AccessFragment.kt
@@ -3,15 +3,25 @@ package com.guga.supp4youapp.presentation.ui.fragment
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.guga.supp4youapp.R
+import com.guga.supp4youapp.data.remote.database.Space
 import com.guga.supp4youapp.databinding.FragmentAccessBinding
 import com.guga.supp4youapp.presentation.ui.adapter.CustomSpinnerAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class AccessFragment : Fragment(R.layout.fragment_access) {
 
     private lateinit var binding: FragmentAccessBinding
+    private val createSpace = Firebase.firestore.collection("create")
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,6 +29,12 @@ class AccessFragment : Fragment(R.layout.fragment_access) {
         binding = FragmentAccessBinding.bind(view)
 
         binding.tvCreateSpace.setOnClickListener {
+            val groupName = binding.tvGroupName.text.toString()
+            val selectedDays = binding.spDays.selectedItem.toString()
+            val selectBeginTime = binding.spStartTime.selectedItem.toString()
+            val selectEndTime = binding.spEndTime.selectedItem.toString()
+            val space = Space(groupName, selectedDays, selectBeginTime, selectEndTime)
+            createSpace(space)
             findNavController().navigate(R.id.action_accessFragment_to_generateFragment)
         }
 
@@ -55,4 +71,18 @@ class AccessFragment : Fragment(R.layout.fragment_access) {
         }
 
     }
+
+    private fun createSpace(space: Space) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            createSpace.add(space).await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), "Successfully create space", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
